8ca36a33ecdb4e2802a6c4d073d5e1402de89d2d
kacper <kacper@Trol-z-Wolki.local>
2018-11-27 00:47:54 +0100

    Splitter contract

---
+++ contracts/Splitter.sol
@@ -0,0 +1,18 @@
+
+pragma solidity ^0.4.25;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+contract TimeConstrainedCounter {
+
+    //aprove
+    //split ktory da trasfer from
+
+    
+    
+    uint32 public value;
+
+    uint256 public startTime;
+    uint256 public endTime;
+
+}
