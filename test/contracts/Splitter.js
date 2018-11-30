6241498c7572aa4616f70d1e38c62eec7c577d39
kasso <kasso@MacBook-Pro-kasso.local>
2018-11-30 01:00:43 +0100

    mac os change

--- test/contracts/Splitter.js
+++ test/contracts/Splitter.js
@@ -8,4 +8,49 @@ import Splitter from '../../build/Splitter.json';
 
 chai.use(chaiAsPromised);
 
-describe('Escrow', () => {});
+describe('Splitter', () => {
+  let provider;
+  let sellerWallet;
+  let buyerWallet;
+  const price = utils.parseUnits('1', 'ether');
+  const doublePrice = price.mul(2);
+  let contractForSeller;
+  let contractForBuyer;
+  let blog;
+
+  before(async () => {
+    provider = createMockProvider();
+    [sellerWallet, buyerWallet] = await getWallets(provider);
+  });
+
+  beforeEach(async () => {
+    contractForSeller = await deployContract(sellerWallet, Splitter, [], {value: doublePrice});
+    contractForBuyer = contractWithWallet(contractForSeller, buyerWallet);
+  });
+
+  describe('construction', () => {
+    it('stores the price as half of the payed amount', async () => {
+      expect(await contractForSeller.price()).to.deep.equal(price);
+    });
+
+    it('fails if odd amount is payed', async () => {
+      await expect(deployContract(sellerWallet, Splitter, [], {value: doublePrice.sub(1)})).to.be.eventually.rejected;
+    });
+  });
+
+  describe('contract cancelation', () => {
+    it('fails if contract is tryed to be canceled by not seller', async () => {
+      await expect(contractForBuyer.cancel()).to.eventually.be.rejected;
+    });
+    it('fails if contract is tryed to be canceled by buyer but is in progress', async () => {
+      contractForBuyer.confirmPurchase({value: doublePrice});
+      await expect(contractForSeller.cancel()).to.be.eventually.rejected;
+    });
+  });
+
+  it('fails if the buyer pays a incorrect amount while confirming purchase', async () => {
+    await expect(contractForBuyer.confirmPurchase({value: 0})).to.eventually.be.rejected;
+    await expect(contractForBuyer.confirmPurchase({value: doublePrice.add(2)})).to.eventually.be.rejected;
+    await expect(contractForBuyer.confirmPurchase({value: price})).to.eventually.be.rejected;
+  });
+});
