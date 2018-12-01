bc1dae7ace62e505f87b90597c568368d07e51f8
Kassolari <wisniewski@o2.pl>
2018-12-01 22:27:08 +0100

    commit

--- test/contracts/Splitter.js
+++ test/contracts/Splitter.js
@@ -11,7 +11,6 @@ describe('Splitter', () => {
   let provider;
   let sellerWallet;
   let buyerWallet;
-<<<<<<< HEAD
   let contractForSeller;
   let contractForBuyer;
 
@@ -21,59 +20,21 @@ describe('Splitter', () => {
   let feeCollector;
   let beneficiaries;
 
-=======
-  const price = utils.parseUnits('1', 'ether');
-  const doublePrice = price.mul(2);
-  let contractForSeller;
-  let contractForBuyer;
-
->>>>>>> 54814f441f1a98d94d1b8fa10523e3beb259711d
   before(async () => {
     provider = createMockProvider();
     [sellerWallet, buyerWallet] = await getWallets(provider);
   });
 
   beforeEach(async () => {
-<<<<<<< HEAD
     // constructor(address[] _beneficiaries, address _feeCollector, ERC20 _token)
     contractForSeller = await deployContract(sellerWallet, Splitter, []);
-=======
-    contractForSeller = await deployContract(sellerWallet, Splitter, [], {value: doublePrice});
->>>>>>> 54814f441f1a98d94d1b8fa10523e3beb259711d
     contractForBuyer = contractWithWallet(contractForSeller, buyerWallet);
   });
 
   describe('construction', () => {
-<<<<<<< HEAD
     it('fails if there is less than 2 people to splitt', async () => {
       expect(await contractForSeller.beneficiaries()).to.be.equal(1);
       // console.log(price);
     });
   });
-=======
-    it('stores the price as half of the payed amount', async () => {
-      expect(await contractForSeller.price()).to.deep.equal(price);
-    });
-
-    it('fails if odd amount is payed', async () => {
-      await expect(deployContract(sellerWallet, Splitter, [], {value: doublePrice.sub(1)})).to.be.eventually.rejected;
-    });
-  });
-
-  describe('contract cancelation', () => {
-    it('fails if contract is tryed to be canceled by not seller', async () => {
-      await expect(contractForBuyer.cancel()).to.eventually.be.rejected;
-    });
-    it('fails if contract is tryed to be canceled by buyer but is in progress', async () => {
-      contractForBuyer.confirmPurchase({value: doublePrice});
-      await expect(contractForSeller.cancel()).to.be.eventually.rejected;
-    });
-  });
-
-  it('fails if the buyer pays a incorrect amount while confirming purchase', async () => {
-    await expect(contractForBuyer.confirmPurchase({value: 0})).to.eventually.be.rejected;
-    await expect(contractForBuyer.confirmPurchase({value: doublePrice.add(2)})).to.eventually.be.rejected;
-    await expect(contractForBuyer.confirmPurchase({value: price})).to.eventually.be.rejected;
-  });
->>>>>>> 54814f441f1a98d94d1b8fa10523e3beb259711d
 });
